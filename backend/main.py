9d6a06e534fa87ec52770f0981b49358f7e4f762
tejasrathod-stack <tejas.rathod@sigmoidanalytics.com>
2026-02-13 07:56:24 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,10 @@ def get_contacts():
 
 @app.route("/create_contact", methods=["POST"])
 def create_contact():
-    first_name = request.json.get("firstName")
-    last_name = request.json.get("lastName")
-    email = request.json.get("email")
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
 
     if not first_name or not last_name or not email:
         return (
